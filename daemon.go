0ac986a1ea36dd3058f95ec81f995a70f8971a22
david latotzky <david-github@latotzky.de>
2019-04-08 23:03:05 +0200

    Cleanup debug output, help and defaults

--- daemon.go
+++ daemon.go
@@ -21,7 +21,7 @@ func dataReceive(conn net.Conn, ch chan string) {
 		if err != nil {
 			break //Client disconnected.
 		}
-		log.Printf("Reading line from socket to channel: %d B \n", len(data))
+		fmt.Printf("Reading line from socket to channel: %d B \n", len(data))
 		ch <- strings.TrimSuffix(data, "\n")
 	}
 	err := conn.Close()
@@ -33,11 +33,11 @@ func dataReceive(conn net.Conn, ch chan string) {
 
 func dataSend(ch chan string, url string) {
 
-	log.Printf("Messages gathered: %d \n", len(ch))
+	// log.Printf("Collected: %d \n", len(ch))
 	num := len(ch)
 	if num == 0 {
 		return
-	};
+	}
 	body := ""
 	total := 0
 	var message string
@@ -59,7 +59,7 @@ func post(data []byte, url string) {
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Print("Failed to send to server.")
-		return;
+		return
 	}
 	defer resp.Body.Close()
 
@@ -77,23 +77,27 @@ func check(err error, message string) {
 
 func getOpts() (int, string, string, time.Duration) {
 	sockPtr := flag.String("socket", "/tmp/.apm.sock", "default \"/tmp/.apm.sock\"")
-	urlPtr := flag.String("url", "http://localhost:8000/simple.php", "e.g. http://localhost:8000/intake/v2/events")
+	urlPtr := flag.String("url", "http://localhost:8200/v1/transmissions", "e.g. , "+
+		"http://localhost:8200/v1/transmissions, http://localhost:8200/v1/errors "+
+		"or http://localhost:8000/intake/v2/events")
 	intervalPtr := flag.String("send-every", "60s", "e.g. 2m, 30s,... ")
 	bufferPtr := flag.Int("buffer", 10000, "max number of lines")
 	flag.Parse()
 
 	intervalLength := *intervalPtr
 	interval, err := time.ParseDuration(intervalLength)
-	check(err, fmt.Sprintf("Sending data every %s.", interval.String()))
+	check(err, fmt.Sprintf("Sending data every %s.", intervalLength))
 	return *bufferPtr, *sockPtr, *urlPtr, interval
 }
 
 func main() {
-	buffer, socket, url, interval:= getOpts()
+	buffer, socket, url, interval := getOpts()
 
 	err := os.Remove(socket)
 	l, err := net.Listen("unix", socket)
-	check(err, "apm daemon is ready.")
+	check(err, "APM daemon is ready.")
+	fmt.Println("Listen to ", socket)
+	fmt.Println("Send to ", url)
 	defer l.Close()
 
 	ch := make(chan string, buffer)
